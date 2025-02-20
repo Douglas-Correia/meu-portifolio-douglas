e1c5cc8262feb001bfc1ebd1f1b3e386eec86024
Douglas-Correia <douglasmestre6@gmail.com>
2025-02-20 11:49:20 -0400

    update: updated curriculo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "@/styles/globals.css";
 import { Header } from "@/components/Header";
-import { useEffect } from "react";
-import AOS from "aos";
 import "aos/dist/aos.css";
 import { AOSClient } from "@/components/AOSClient";
 
